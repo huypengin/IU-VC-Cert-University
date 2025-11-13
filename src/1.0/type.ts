955f01745fdf92e6f717200007ef2afc05f8aeb8
Nguyen Anh Thang <nganhthang147@gmail.com>
2025-11-13 22:26:09 +0700

    Init commit

---
+++ src/1.0/type.ts
@@ -0,0 +1,40 @@
+export type VCModel = {
+    '@context': string | string[];
+    type: string | string[];
+    credentialSubject: Record<string, any>;
+    issuer: string | Record<string, any>;
+    issuanceDate: string;
+    expirationDate?: string;
+    proof?: Record<string, any>;
+}
+
+export type CredentialStatus = {
+    id: string;
+    type: string;
+    statusPurpose: string;
+    statusListCredential: string;
+    statusListIndex: string;
+}
+
+export type ProofModel = {
+    type: string;
+    cryptosuite: string;
+    proofPurpose: string;
+    verificationMethod: string;
+    merkleReceipt: MerkleReceipt;
+}
+
+export type MerkleReceipt = {
+    chainId: string;
+    contractAddress: string;
+    merkleRoot: string;
+    anchorTx: string;
+    hashAlg: string;
+    leftEncoding: typeof LeftEncoding;
+}
+
+export const LeftEncoding = {
+    credentialID: 'CredentialID',
+    componentType: 'componentType',
+    content: 'content',
+}
---
+++ src/1.0/wrapper.ts
@@ -0,0 +1,5 @@
+/**
+ * This wrapper is used to input data to the IUSmartCert document.
+ * */
+
+
